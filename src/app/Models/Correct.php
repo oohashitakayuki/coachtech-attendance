df4f1dd343f70158fe5db6ecedfa98c1842a7b11
oohashitakayuki <ssg675vl@docomo.ne.jp>
2025-06-01 14:03:48 +0900

    リレーションを設定

---
+++ src/app/Models/Correct.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Correct extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'attendance_id', 'comment', 'approved_at'
+    ];
+
+    public function attendance()
+    {
+        return $this->belongsTo(Attendance::class);
+    }
+}
---
+++ src/app/Models/Rest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rest extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'attendance_id', 'break_start', 'break_end', 'break_time'
+    ];
+
+    public function attendance()
+    {
+        return $this->belongsTo(Attendance::class);
+    }
+}

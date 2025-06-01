e41c3ce94f62985ab196838c9ebbe0187fa037dd
oohashitakayuki <ssg675vl@docomo.ne.jp>
2025-06-01 14:42:47 +0900

    First commit

---
+++ src/app/Http/Middleware/AdminAuth.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!Auth::guard('admin')->check()) {
+            return redirect('/admin/login');
+        }
+
+        return $next($request);
+    }
+}
